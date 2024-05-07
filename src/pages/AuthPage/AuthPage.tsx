4c20333775b95a37e22b2660558670117dcff4f5
Sofia <vdo-sofia@mail.ru>
2024-05-07 09:35:58 +0300

    feat: rename page to AuthPage, remove defaultProps

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,14 +1,14 @@
 import Logo from '../../shared/assets/images/logo.svg';
-import Modal from '../../shared/ui/Modal/Modal';
 import AuthBackground from '../../shared/assets/images/auth_background.webp';
-import cls from './LoginPage.module.scss';
 
-export const LoginPage = () => {
+import cls from './AuthPage.module.scss';
+
+export const AuthPage = () => {
   return (
     <main className={cls.main}>
       <img className={cls.background} src={AuthBackground} alt="background" />
       <Logo />
-      <Modal>hello</Modal>
+      <div className={cls.Card} />
     </main>
   );
 };
