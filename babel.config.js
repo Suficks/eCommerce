3212543b1a59d7b808507869996fca1cd25bff36
samael-god <nikita.95_@mail.ru>
2024-05-09 09:46:23 +0300

    feat: update tests (#17)
    * feat: update jest config, add tests
    
    * feat: update tests due to redux implementation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,2 @@
 module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}], ['@babel/preset-react', { runtime: 'automatic' }]],
-};
+    presets: [['@babel/preset-env', {targets: {node: 'current'}}], ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],};
