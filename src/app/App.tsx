8726ae89395ef4189550acd4fd994405315bf1b8
NatashaKLe <149941006+NatashaKLe@users.noreply.github.com>
2024-05-10 11:22:03 +0300

    feat: add page not found and add scss additional imports (#19)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import { LoginPage } from '@/pages/LoginPage/LoginPage';
-import './styles/index.scss';
 
 export const App = () => {
   return <LoginPage />;
--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,16 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
+  css: {
+    devSourcemap: true,
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[local]',
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/app/styles/variables/global";@import "./src/app/styles/fonts/fonts";@import "./src/app/styles/mixins/mixins";@import "./src/app/styles";@import "reset-css";`,
+      },
+    },
+  },
 });
