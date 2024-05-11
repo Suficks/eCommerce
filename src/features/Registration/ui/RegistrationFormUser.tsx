532044a06794686a572e104317c9344888936154
samael-god <nikita.95_@mail.ru>
2024-05-11 10:40:13 +0300

    fix: implement error message component

--- src/features/Registration/ui/RegistrationFormUser.tsx
+++ src/features/Registration/ui/RegistrationFormUser.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/shared/ui/button/button';
 import { Validation } from '@/shared/const/Validation';
 
 import cls from './RegistrationForm.module.scss';
+import { AppError } from '@/shared/ui/AppError/AppError';
 
 export interface RegistrationFormProps {
   className?: string;
@@ -36,67 +37,58 @@ export const RegistrationFormUser = memo(
         onSubmit={handleSubmit(onSubmit)}
         className={classNames(cls.form, className)}
       >
-        <Input
-          register={register('email', {
-            required: 'Enter your email!',
-            pattern: {
-              value: Validation.email,
-              message: 'Invalid email address',
-            },
-          })}
-          type="email"
-          placeholder="email"
-          label="Email"
-          className={cls.input}
-        />
-        <div className={cls.error}>
-          {errors?.email && (
-            <p className={cls.error__message}>
-              {errors.email?.message || 'Error!'}
-            </p>
-          )}
+        <div className={cls.input__wrapper}>
+          <Input
+            register={register('email', {
+              required: 'Enter your email!',
+              pattern: {
+                value: Validation.email,
+                message: 'Invalid email address',
+              },
+            })}
+            type="email"
+            placeholder="email"
+            label="Email"
+            className={cls.input}
+          />
+          {errors?.email &&
+            AppError({ text: errors.email?.message || 'Error!' })}
         </div>
-        <Input
-          placeholder="password"
-          label="Password"
-          className={cls.input}
-          type="password"
-          register={register('password', {
-            required: 'Enter your password!',
-            pattern: {
-              value: Validation.password,
-              message:
-                'English only. Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number',
-            },
-          })}
-        />
-        <div className={cls.error}>
-          {errors?.password && (
-            <p className={cls.error__message}>
-              {errors.password?.message || 'Error!'}
-            </p>
-          )}
+        <div className={cls.input__wrapper}>
+          <Input
+            placeholder="password"
+            label="Password"
+            className={cls.input}
+            type="password"
+            register={register('password', {
+              required: 'Enter your password!',
+              pattern: {
+                value: Validation.password,
+                message:
+                  'English only. Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number',
+              },
+            })}
+          />
+          {errors?.password &&
+            AppError({ text: errors.password?.message || 'Error!' })}
         </div>
-        <Input
-          placeholder="password_repeat"
-          label="Repeat password"
-          className={cls.input}
-          type="password"
-          register={register('passwordConfirm', {
-            required: 'Confirm your password!',
-            validate: (value) =>
-              value === getValues('password') || 'Must match the password.',
-          })}
-        />
-        <div className={cls.error}>
-          {errors?.passwordConfirm && (
-            <p className={cls.error__message}>
-              {errors.passwordConfirm?.message || 'Error!'}
-            </p>
-          )}
+        <div className={cls.input__wrapper}>
+          <Input
+            placeholder="password_repeat"
+            label="Repeat password"
+            className={cls.input}
+            type="password"
+            register={register('passwordConfirm', {
+              required: 'Confirm your password!',
+              validate: (value) =>
+                value === getValues('password') || 'Must match the password.',
+            })}
+          />
+          {errors?.passwordConfirm &&
+            AppError({ text: errors.passwordConfirm?.message || 'Error!' })}
         </div>
         <Button
-          text="Login"
+          text="Register"
           className={cls.button}
           onClick={handleSubmit(onSubmit)}
         />
