d6cc86ee8750842fe04a050e9a4b06293ed45320
samael-god <nikita.95_@mail.ru>
2024-05-11 08:51:10 +0300

    fix: change snapshot test to render

--- __test__/components.test.tsx
+++ __test__/components.test.tsx
@@ -1,38 +1,35 @@
-import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import { Button } from '@/shared/ui/button/button';
 import { AppLink } from '@/shared/ui/AppLink/AppLink';
-import { App } from '@/app/App';
 import { setupStore } from '@/app/store/config/store';
+import { LoginForm } from '@/features/Login';
 
 describe('Components render test', () => {
   it('should render a button', () => {
-    const component = renderer.create(<Button text="Valera" />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    render(<Button text="Valera" />);
+    expect(screen.getByText('Valera')).toBeInTheDocument();
   });
 
   it('should render a link', () => {
-    const component = renderer.create(
+    render(
       <BrowserRouter>
-        <AppLink text="valeraa" to="/registration" />
+        <AppLink text="Valera" to="/registration" />
       </BrowserRouter>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(screen.getByText('Valera')).toHaveRole('link');
   });
 
-  it('should render an app', () => {
+  it('should render a login form', () => {
     const store = setupStore();
-    const component = renderer.create(
+    render(
       <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <LoginForm data-testid="username" />
       </Provider>,
+      { wrapper: BrowserRouter },
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(screen.getAllByText(/login/i)).toHaveLength(2);
   });
 });
