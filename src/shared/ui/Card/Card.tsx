26b6b5d0db55e0cc439bd1aa468dbf39ba672d0b
Sofia <vdo-sofia@mail.ru>
2024-05-14 14:28:55 +0300

    feat: add Card component

---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import cls from './Card.module.scss';
+import { Icon } from '../Icon/Icon';
+
+interface CardProps {
+  className?: string;
+  image?: React.VFC<React.SVGProps<SVGSVGElement>>;
+  width?: number;
+  green?: boolean;
+  text?: string;
+}
+
+export const Card = (props: CardProps) => {
+  const { className, text, image, green, width } = props;
+  return (
+    <div className={classNames(cls.Card, className)} style={{ width }}>
+      {image && <Icon Svg={image} />}
+      {text && (
+        <p className={classNames(cls.text, { [cls.green]: green })}>{text}</p>
+      )}
+    </div>
+  );
+};
