d0747a5bbf06dba0a23c3e0d8528cc317e08b63b
Sofia <vdo-sofia@mail.ru>
2024-05-14 15:58:53 +0300

    feat: add infoBlock

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,10 @@
 import classNames from 'classnames';
 
-import { Button } from '@/shared/ui/button/button';
 import MainLeaf from '@/shared/assets/images/main_leaf.svg';
+import Background from '@/shared/assets/images/info_background.jpg';
+import { Button } from '@/shared/ui/button/button';
 import { Icon } from '@/shared/ui/Icon/Icon';
+import { Card } from '@/shared/ui/Card/Card';
 
 import cls from './MainPage.module.scss';
 
@@ -19,6 +21,15 @@ export const MainPage = (props: MainPageProps) => {
         <h1 className={cls.subtitle}>Organic</h1>
         <Button text="Explore More" transparent className={cls.button} />
         <Icon Svg={MainLeaf} className={cls.mainLeaf} />
+      </div>
+      <div className={cls.infoBlock}>
+        <Card
+          width={1200}
+          className={cls.card}
+          text="Shop guilt-free knowing that our eco-friendly e-commerce platform is committed to reducing environmental 
+          impact and promoting sustainable practices in every step of the shopping process"
+        />
+        <img src={Background} className={cls.infoBackground} alt="background" />
       </div>
     </main>
   );
--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -13,7 +13,10 @@ interface CardProps {
 export const Card = (props: CardProps) => {
   const { className, text, image, green, width } = props;
   return (
-    <div className={classNames(cls.Card, className)} style={{ width }}>
+    <div
+      className={classNames(cls.Card, className)}
+      style={{ maxWidth: width }}
+    >
       {image && <Icon Svg={image} />}
       {text && (
         <p className={classNames(cls.text, { [cls.green]: green })}>{text}</p>
