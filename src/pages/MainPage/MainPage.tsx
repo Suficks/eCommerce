e6c2048f2760b7cb6e5185aa3ca92f8055d2721d
Natashakle <ataska91@gmail.com>
2024-05-15 01:27:56 +0300

    feat: add block add info in main, remove the name field from the login page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,6 +5,9 @@ import Background from '@/shared/assets/images/info_background.webp';
 import { Button } from '@/shared/ui/button/button';
 import { Icon } from '@/shared/ui/Icon/Icon';
 import { Card } from '@/shared/ui/Card/Card';
+import Bottle from '@/shared/assets/images/bottle.svg';
+import Cloud from '@/shared/assets/images/cloud.svg';
+import Hands from '@/shared/assets/images/hands.svg';
 
 import cls from './MainPage.module.scss';
 
@@ -31,6 +34,31 @@ export const MainPage = (props: MainPageProps) => {
         />
         <img src={Background} className={cls.infoBackground} alt="background" />
       </div>
+      <section className={cls.addInfo}>
+        <div className={cls.whyUs}>
+          <p className={cls.question}>Why Buy from Us?</p>
+          <div className={cls.ecologicalBenefits}>
+            <div className={cls.oneBenefit}>
+              <Card image={Bottle} width={205} text="641,698 +" green />
+              <p>Zero Plastic Products Sold</p>
+            </div>
+            <div className={cls.oneBenefit}>
+              <Card image={Cloud} width={205} text="42,780" green />
+              <p>Tons Carbon Emission Prevented</p>
+            </div>
+            <div className={cls.oneBenefit}>
+              <Card image={Hands} width={205} text="50+" green />
+              <p>Livelihoods Created</p>
+            </div>
+          </div>
+        </div>
+        <div className={cls.featuredProducts}>
+          <p className={cls.question}>Featured Products</p>
+          <div className={cls.products}>
+            <Button text="Shop more" className="buttonMore" />
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -6,5 +6,7 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
 }
 
 export const Icon = ({ className, Svg, ...otherProps }: IconProps) => (
-  <Svg className={classNames(className)} {...otherProps} />
+  <div>
+    <Svg className={classNames(className)} {...otherProps} />
+  </div>
 );
