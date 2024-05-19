b8d9db49d7f3173987337cca10761a2c43fc87a8
samael-god <nikita.95_@mail.ru>
2024-05-19 16:43:25 +0300

    fix: remove unnecessary error cases

--- src/features/Registration/model/services/signUpUser.ts
+++ src/features/Registration/model/services/signUpUser.ts
@@ -7,7 +7,7 @@ import { ValidationErrors } from '@/shared/const/Validation';
 export const signUpUserThunk = createAsyncThunk(
   'signUp/signUpUserThunk',
   async (signUpData: SubmitData) => {
-    const { email, alreadyExist, serverError, password } = ValidationErrors;
+    const { alreadyExist, serverError } = ValidationErrors;
     try {
       const response = await signUpUser(signUpData);
 
@@ -15,17 +15,6 @@ export const signUpUserThunk = createAsyncThunk(
     } catch (e) {
       const error = e as Error;
       switch (error.message) {
-        case 'The provided value is not a valid email': {
-          // скорее всего cause можно вообще убрать
-          throw new Error(email.error, {
-            cause: 'emailError',
-          });
-        }
-        case `'password' should not be empty.`: {
-          throw new Error(password.required, {
-            cause: 'passwordError',
-          });
-        }
         case 'There is already an existing customer with the provided email.': {
           throw new Error(alreadyExist.error, {
             cause: 'emailError',
