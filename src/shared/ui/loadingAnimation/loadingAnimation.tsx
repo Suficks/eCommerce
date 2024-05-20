6df0dfc8c4610a60835b3be3865f0e9e81888196
Sofia <vdo-sofia@mail.ru>
2024-05-20 16:49:53 +0300

    feat: add loading animation

--- src/shared/ui/loadingAnimation/loadingAnimation.tsx
+++ src/shared/ui/loadingAnimation/loadingAnimation.tsx
@@ -1,13 +1,15 @@
-import cls from './loadingAnimation.module.scss';
+import { Icon } from '../Icon/Icon';
 import Leaf from '@/shared/assets/images/leaf.svg';
+
+import cls from './loadingAnimation.module.scss';
 
 export const LoadingAnimation = () => {
   return (
     <div className={cls.wrapperAnimation}>
-      <Leaf />
-      <Leaf />
-      <Leaf />
-      <Leaf />
+      <Icon Svg={Leaf} className={cls.leaf} />
+      <Icon Svg={Leaf} className={cls.leaf} />
+      <Icon Svg={Leaf} className={cls.leaf} />
+      <Icon Svg={Leaf} className={cls.leaf} />
     </div>
   );
 };
