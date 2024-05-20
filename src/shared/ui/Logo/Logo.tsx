f7e3d9ea5b7aedc8572132937b0a302f0b2b8e22
Natashakle <ataska91@gmail.com>
2024-05-20 16:33:56 +0300

    feat: add logo component to login page

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,12 +1,17 @@
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
 import { Routes } from '@/app/providers/RouterConfig/RouteConfig';
 import LogoSVG from '@/shared/assets/images/logo.svg';
 import cls from './Logo.module.scss';
 
-export const Logo = () => {
+interface LogoProps {
+  className?: string;
+}
+
+export const Logo = ({ className }: LogoProps) => {
   return (
-    <Link to={Routes.MAIN} className={cls.logo}>
+    <Link to={Routes.MAIN} className={classNames(cls.logo, className)}>
       <LogoSVG />
     </Link>
   );
