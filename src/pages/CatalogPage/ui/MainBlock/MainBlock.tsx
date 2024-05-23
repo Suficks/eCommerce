7335d54adc5a13ec5fe1cfc027709d0026a8c61d
Sofia <vdo-sofia@mail.ru>
2024-05-23 17:33:53 +0300

    feat: add slider to mainBlock on catalogPage

--- src/pages/CatalogPage/ui/MainBlock/MainBlock.tsx
+++ src/pages/CatalogPage/ui/MainBlock/MainBlock.tsx
@@ -1,14 +1,13 @@
 import classNames from 'classnames';
-
 import { useEffect, useState } from 'react';
+
 import { AppLink } from '@/shared/ui/AppLink/AppLink';
-import { Card } from '@/shared/ui/Card/Card';
 import { useAppDispatch } from '@/shared/hooks/redux';
-import Product_3 from '@/shared/assets/images/product_3.png';
 import { fetchCategories } from '../../model/services/fetchCategories';
+import { CategoryCustom } from '@/shared/api/types/apiTypes';
+import { SliderComponent } from './Slider';
 
 import cls from './MainBlock.module.scss';
-import { CategoryCustom } from '@/shared/api/types/apiTypes';
 
 interface MainBlockProps {
   className?: string;
@@ -30,22 +29,17 @@ export const MainBlock = ({ className }: MainBlockProps) => {
     <section className={classNames(cls.MainBlock, className)}>
       <ul className={cls.list}>
         {categories.map(({ parent }) => (
-          <li key={parent.id} className={cls.item}>
+          <AppLink
+            to={`/catalog/${parent.path}`}
+            key={parent.id}
+            className={cls.item}
+          >
             {parent.name}
-          </li>
+          </AppLink>
         ))}
       </ul>
       <div className={cls.separator} />
-      <Card className={classNames(cls.card)}>
-        <img src={Product_3} alt="" />
-        <div className={cls.wrapper}>
-          <h1 className={cls.title}>Karigar</h1>
-          <h1 className={cls.subtitle}>Up to 10% off Voucher</h1>
-          <AppLink underlined to="/main">
-            Shop Now
-          </AppLink>
-        </div>
-      </Card>
+      <SliderComponent />
     </section>
   );
 };
