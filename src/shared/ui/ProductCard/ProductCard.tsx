079b0c359084ef3594079461fdc412b96de8e056
Sofia <vdo-sofia@mail.ru>
2024-05-24 13:58:34 +0300

    feat: add MathRandom function and productCard component

--- src/shared/ui/ProductCard/ProductCard.tsx
+++ src/shared/ui/ProductCard/ProductCard.tsx
@@ -8,24 +8,33 @@ import cls from './ProductCard.module.scss';
 
 interface ProductCardProps {
   className?: string;
+  onClick: () => void;
   image: string;
   name: string;
-  price: number;
+  price?: number;
   sale?: number;
   stars: number;
   reviews: number;
 }
 
 export const ProductCard = (props: ProductCardProps) => {
-  const { className, name, price, sale, stars, reviews, image } = props;
+  const { className, name, price, sale, stars, reviews, image, onClick } =
+    props;
+
   return (
-    <div className={classNames(cls.ProductCard, className)}>
-      <Card width={209} className={cls.card} image={image} />
+    <button
+      type="button"
+      className={classNames(cls.ProductCard, className)}
+      onClick={onClick}
+    >
+      <Card className={cls.card} image={image} />
       <div className={cls.info}>
         <p>{name}</p>
         <div className={cls.wrap}>
           {sale && <span className={cls.sale}>{`$${sale}`}</span>}
-          <span className={cls.price}>{`$${price}`}</span>
+          <span className={classNames(cls.price, { [cls.salePrice]: sale })}>
+            {`$${price}`}
+          </span>
         </div>
         <div className={cls.wrap}>
           <div className={cls.stars_wrap}>
@@ -36,6 +45,6 @@ export const ProductCard = (props: ProductCardProps) => {
           <span className={cls.reviews}>{`(${reviews})`}</span>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
