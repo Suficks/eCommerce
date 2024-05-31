45f7ac448155b866d05050a7ab412cb9f47af778
samael-god <nikita.95_@mail.ru>
2024-05-31 12:44:31 +0300

    feat: add api request for add/edit/delete address

---
+++ src/shared/api/requests/editCustomerAddress.ts
@@ -0,0 +1,89 @@
+import { apiRoot } from '../BuildClient';
+import { countriesList } from '@/shared/const/Countries';
+import { LocalStorageKeys } from '@/shared/const/LocalStorage';
+
+interface EditAddressProps {
+  ID: string;
+  version: number;
+  addressType: 'Shipping' | 'Billing';
+  addressId: string;
+  country: string;
+  city: string;
+  postal: string;
+  street: string;
+  isDefault: boolean;
+}
+
+export async function editCustomerAddress(
+  props: EditAddressProps,
+): Promise<unknown> {
+  const {
+    ID,
+    version,
+    addressId,
+    country,
+    city,
+    postal,
+    street,
+    isDefault,
+    addressType,
+  } = props;
+  const countryAbbr =
+    countriesList.find(({ name }) => name === country)?.abbr ?? '';
+
+  try {
+    let response = await apiRoot
+      .customers()
+      .withId({ ID })
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'changeAddress',
+              addressId,
+              address: {
+                country: countryAbbr,
+                city,
+                postalCode: postal,
+                streetName: street,
+              },
+            },
+          ],
+        },
+      })
+      .execute();
+    localStorage.setItem(
+      LocalStorageKeys.VERSION,
+      JSON.stringify(response.body.version),
+    );
+    if (isDefault) {
+      const newVersion = Number(localStorage.getItem(LocalStorageKeys.VERSION));
+      response = await apiRoot
+        .customers()
+        .withId({ ID })
+        .post({
+          body: {
+            version: newVersion,
+            actions: [
+              {
+                action: `setDefault${addressType}Address`,
+                addressId,
+              },
+            ],
+          },
+        })
+        .execute();
+      localStorage.setItem(
+        LocalStorageKeys.VERSION,
+        JSON.stringify(response.body.version),
+      );
+    }
+    return response;
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new Error(e.message);
+    }
+  }
+  return undefined;
+}
