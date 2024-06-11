5a9d343545c54a8a8294386560e854559da3eecd
samael-god <nikita.95_@mail.ru>
2024-06-11 10:58:42 +0000

    feat: test transition

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,16 @@
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useLocation } from 'react-router-dom';
+import './AppRouterTransitions.scss';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { AppRouter } from '@/app/providers/ui/AppRouter';
 
 export const App = () => {
-  return <RouterProvider router={AppRouter} />;
+  const location = useLocation();
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="page" timeout={2000}>
+        <RouterProvider router={AppRouter} />
+      </CSSTransition>
+    </TransitionGroup>
+  );
 };
